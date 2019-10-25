8c8f3dab56cf28cedf2d72fe757e20e8f1097fd9
GenesisSam <samuel940401@gmail.com>
2019-10-25 11:32:05 -0700

    Build & deploy to gh-pages

---
+++ build/precache-manifest.aa46abfa61d1af6bb334fe5f0f5c0f81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2704eb5d10a13e48f71fc4a2b2739acf",
+    "url": "/battle-2048-grid/index.html"
+  },
+  {
+    "revision": "e927ce245941071157f0",
+    "url": "/battle-2048-grid/static/js/2.9e18e973.chunk.js"
+  },
+  {
+    "revision": "fae6dd4b8a141af5121c",
+    "url": "/battle-2048-grid/static/js/main.e8471b47.chunk.js"
+  },
+  {
+    "revision": "9d0cf0133efe0e832acc",
+    "url": "/battle-2048-grid/static/js/runtime-main.3e240f15.js"
+  }
+]);
